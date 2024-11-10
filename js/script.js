bb917b1de817c203076f81a90e10bddb48f818d6
JAKKU <148993346+Jvkku@users.noreply.github.com>
2024-11-10 18:13:21 +0100

    liampiar code

--- js/script.js
+++ js/script.js
@@ -2,19 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuButton = document.getElementById("menuButton");
     const menuOptions = document.getElementById("menuOptions");
 
-    // Función para alternar el menú
     function toggleMenu() {
         menuOptions.classList.toggle("d-none");
         menuButton.classList.toggle("active"); // Cambiar color del botón activo
     }
 
-    // Evento de clic en el botón de menú
     menuButton.addEventListener("click", function (event) {
         event.stopPropagation();
         toggleMenu();
     });
 
-    // Cerrar el menú si se hace clic fuera o se hace scroll
     document.addEventListener("click", function (event) {
         if (!menuOptions.classList.contains("d-none") && !menuButton.contains(event.target)) {
             menuOptions.classList.add("d-none");
